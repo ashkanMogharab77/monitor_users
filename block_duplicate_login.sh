1c022015796f565f49b5c3a70c63b09560b74939
ashkanMogharab77 <ashkanmogharab7723@gmail.com>
2025-08-08 09:21:32 +0330

    supplementary files for main branch

---
+++ block_duplicate_login.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BLOCKED_FILE="/root/monitor_users/blocked_users_ips.txt"
+
+if ! grep -q "^$PAM_USER:" "$PORT_FILE"; then
+    exit 0
+fi
+
+valid_sessions=$(grep "^$PAM_USER:" "$PORT_FILE" | cut -d':' -f2 )
+
+session_count=$(ps aux | awk -v u="$PAM_USER" '$1 == u && $0 ~ ("sshd: " u)' | wc -l)
+
+if [ "$session_count" -ge "$valid_sessions" ]; then
+    exit 1
+fi
+
+pids=$(ps aux | awk -v u="$PAM_USER" '$1 == "sshd" && $0 ~ ("sshd: " u) { print $2 }')
+
+for pid in $pids; do
+    ip=$(ss -tunp | grep "pid=$pid" | grep 6040 | awk '{print $6}' | cut -d':' -f1)
+    while IFS=: read -r user line_ip; do
+        if [[ "$user" == "$PAM_USER" && "$line_ip" == "$ip" ]]; then
+            exit 1
+        fi
+    done < <(grep "$ip" "$BLOCKED_FILE")
+done
+
+exit 0
