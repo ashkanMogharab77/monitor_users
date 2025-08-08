9aaa07ca091acce46e8159ddad347999e625ec97
ashkanMogharab77 <ashkanmogharab7723@gmail.com>
2025-08-08 11:17:23 +0330

    add path of ports.txt

--- block_duplicate_login.sh
+++ block_duplicate_login.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 BLOCKED_FILE="/root/monitor_users/blocked_users_ips.txt"
+PORT_FILE="/root/monitor_users/ports.txt"
+port = "6040"
 
 if ! grep -q "^$PAM_USER:" "$PORT_FILE"; then
     exit 0
 fi
 
-valid_sessions=$(grep "^$PAM_USER:" "$PORT_FILE" | cut -d':' -f2 )
-
+valid_sessions=$(grep "^$PAM_USER:" "$PORT_FILE" | cut -d':' -f2)
 session_count=$(ps aux | awk -v u="$PAM_USER" '$1 == u && $0 ~ ("sshd: " u)' | wc -l)
 
 if [ "$session_count" -ge "$valid_sessions" ]; then
@@ -17,7 +18,7 @@ fi
 pids=$(ps aux | awk -v u="$PAM_USER" '$1 == "sshd" && $0 ~ ("sshd: " u) { print $2 }')
 
 for pid in $pids; do
-    ip=$(ss -tunp | grep "pid=$pid" | grep 6040 | awk '{print $6}' | cut -d':' -f1)
+    ip=$(ss -tunp | grep "pid=$pid" | grep "$port" | awk '{print $6}' | cut -d':' -f1)
     while IFS=: read -r user line_ip; do
         if [[ "$user" == "$PAM_USER" && "$line_ip" == "$ip" ]]; then
             exit 1
